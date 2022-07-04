193fc3eb47ee92d4fd0b7faa0d2904969c649f5c
Sai Nikhil <gsainikhil13@gmail.com>
2022-07-04 19:18:12 +0530

    new commit

--- fibonacci_generator.py
+++ fibonacci_generator.py
@@ -1,6 +1,4 @@
-from re import A
-
-
+# To generate fibonacci number less than n
 def fibonacci_generator():
     a,b=0,1
     while True:
@@ -8,8 +6,13 @@ def fibonacci_generator():
         a,b=b,a+b
 
 g = fibonacci_generator()
-count=1
-n= int(input('enter number of fibonacci number required : '))
-while count <= n:
-    print(next(g))
-    count= count+1
+n= int(input('enter number up to which the fibonacci number are required : '))
+for x in g:
+    if x<= n:
+        print(x)
+    else:
+        break
+        
+
+
+
