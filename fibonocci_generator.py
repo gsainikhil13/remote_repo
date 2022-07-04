9b3d9873f75f26fccb80700aea45956db4cd00d1
Sai Nikhil <gsainikhil13@gmail.com>
2022-07-04 19:27:38 +0530

    second commit

--- fibonocci_generator.py
+++ fibonocci_generator.py
@@ -13,3 +13,25 @@ for x in g:
     else:
         break
  
+
+
+ 
+
+
+
+ 
+# To generate first n fibonacci numbers
+def fibonacci_generator():
+    a,b=0,1
+    while True:
+        yield a
+        a,b=b,a+b
+
+g = fibonacci_generator()
+count=1
+n= int(input('enter number of fibonacci number required : '))
+while count <= n:
+    print(next(g))
+    count= count+1
+
+
